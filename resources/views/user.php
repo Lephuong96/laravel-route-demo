58a3812a2c15fd295d83424f0da219ed3511609d
Lephuong96 <bigankroi@gmail.com>
2019-04-26 15:46:37 +0700

    first commit

---
+++ resources/views/user.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Route</title>
+</head>
+<body>
+<h1>Welcome <?= $name ?>!</h1>
+<h2>Chào mừng bạn <?= $name ?> đến với trang web của tôi</h2>
+
+</body>
+</html>
